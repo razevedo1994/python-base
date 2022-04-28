20b279aa40c3e84a0a4979653026679d7c1f2328
razevedo1994 <razevedo.contato@gmail.com>
2022-04-28 01:02:05 -0300

    refactor: refatora a tabuada

--- day_2_tipos_de_dados_e_protocolos/tabuada.py
+++ day_2_tipos_de_dados_e_protocolos/tabuada.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
-"""Imprime a tabuada do 1 ao 10."""
-__version__ = "0.1.0"
+"""Imprime a tabuada do 1 ao 10.
+
+---Tabuada do 1---
+    1 x 1 = 1
+    2 x 1 = 2
+    3 x 1 = 3
+...
+##################
+---Tabuada do 2---
+    2 x 1 = 2
+    2 x 2 = 4
+...
+##################
+"""
+__version__ = "0.1.1"
 __author__ = "Rodrigo Azevedo"
 
 numeros = list(range(1, 11))
 
-for numero in numeros:
-    print("Tabuada do:", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("----------")
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print("#" * 18)
