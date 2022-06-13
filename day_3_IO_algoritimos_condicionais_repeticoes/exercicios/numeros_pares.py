b87c1151cb4067f3012277263b97e1047c9043d0
razevedo1994 <razevedo.contato@gmail.com>
2022-06-13 20:27:10 -0300

    exercicio numeros_pares

---
+++ day_3_IO_algoritimos_condicionais_repeticoes/exercicios/numeros_pares.py
@@ -0,0 +1,16 @@
+#!usr/bin/env python3
+"""
+Construa um programa que imprime os numeros pares de 1 a 200
+
+ex
+`python numeros_pares.py`
+
+>> 2
+>> 4
+>> 6
+>> 8
+...
+"""
+for numero in range(1, 201):
+    if numero % 2 == 0:
+        print(numero)
