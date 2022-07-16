af0512786ba84bd1f12f4a4b47b203748b476e86
razevedo1994 <razevedo.contato@gmail.com>
2022-07-16 18:09:16 -0300

    refactor: refatora utilizando funcao

--- day_3/exception/errors.py
+++ day_3/exception/errors.py
@@ -2,15 +2,25 @@
 
 import sys
 import os
+import logging
 
-try:
-    names = open("names.txt").readlines()
-except FileNotFoundError as e:
-    print(f"{str(e)}")
+log = logging.Logger("errors")
 
 
-try:
-    print(names[2])
-except:
-    print("[Error] Missing name in the list.")
-    sys.exit(1)
+def try_to_open_a_file(filepath, retry=1) -> list:
+    """Tries to open a file, if error, retries n times."""
+    for attempt in range(1, retry + 1):
+        try:
+            return open(filepath).readline()
+        except FileNotFoundError as e:
+            log.error("ERRO: %s", e)
+        else:
+            print("Sucesso!")
+        finally:
+            print("Execute isso sempre!")
+
+    return []
+
+
+for line in try_to_open_a_file("names.txt", retry=5):
+    print(line)
